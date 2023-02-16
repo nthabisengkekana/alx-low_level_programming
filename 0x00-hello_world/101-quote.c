365908f6833ec560d598dfbce6aea30ef9bb52dd
Nthabiseng Kekana <nthabisengkekana54@yahoo.com>
2023-02-16 05:28:22 -0800

    Task 8 Quote C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A C program that prints a line to the standard error
- * Return: 1 (Successful)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }

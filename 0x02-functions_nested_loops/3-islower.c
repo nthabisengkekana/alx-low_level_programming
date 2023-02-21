88106c94afaada8991bbc132fc3f4ab376dd7d96
Nthabiseng Kekana <nthabisengkekana54@yahoo.com>
2023-02-21 09:35:08 -0800

    Task 3 Is Lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,16 +7,8 @@
  */
 int _islower(int c)
 {
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-
 	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
-		return (0);
+		return (Id);
 }
